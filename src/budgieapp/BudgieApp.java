6afa3940405262937e69dea348df02bb6a507a35
jpierik <jpierik@oakland.edu>
2016-11-16 06:39:03 -1000

    Added BudgieApp to repository

---
+++ src/budgieapp/BudgieApp.java
@@ -0,0 +1,13 @@
+
+package budgieapp;
+
+
+public class BudgieApp {
+    public static void main(String[] args) {
+        System.out.println("hellow worlds");
+
+
+        int one = 1;
+
+    }    
+}
