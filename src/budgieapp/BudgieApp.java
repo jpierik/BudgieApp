8c24c9feb0ff3438ce290b26a7d3cfa0e3b677b3
jpierik <jpierik@oakland.edu>
2016-11-16 12:50:35 -0600

    another test

--- src/budgieapp/BudgieApp.java
+++ src/budgieapp/BudgieApp.java
@@ -3,15 +3,7 @@ package budgieapp;
 
 
 public class BudgieApp {
-    public static void main(String[] args) {
-        System.out.println("hellow worlds");
+    
 
-
-        int one = 1;
-        int two = 2;
-        int sum = 0;
-        sum = one + two;
-        System.out.println("hi");
-
-    }    
+       
 }
